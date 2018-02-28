1c5a75432db1e7638fbbcbeec130313c39e0c9ec
SAE\snixon <1006462@student.sae.edu.au>
2018-02-28 20:49:36 +1000

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    [SerializeField]
+    int speed;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.rotation.eulerAngles.y < rightLimit)
+            {
+                transform.Rotate(new Vector3(0, 1, 0) * (speed * Time.deltaTime));
+            }
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.rotation.eulerAngles.y > leftLimit)
+            {
+                transform.Rotate(new Vector3(0, 1, 0) * (-speed * Time.deltaTime));
+            }
+        }
+    }
+}//bless your socks!!!
